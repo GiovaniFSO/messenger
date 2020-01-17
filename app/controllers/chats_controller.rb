3a9167cd6fc12d04bfd65242aa269d866413438f
Giovani Fernandes <giovani_oli@hotmail.com>
2020-01-17 00:58:28 -0300

    created show chat

---
+++ app/controllers/chats_controller.rb
@@ -0,0 +1,6 @@
+class ChatsController < ApplicationController
+
+  def show
+    @user = User.find(params[:id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :contacts, only: %i(index new create destroy)
+  resources :chats, only: %i(index show)
   root to: 'home#index'
 end

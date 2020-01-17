a9acf6f7760eac84620d6396f69a860e3693b76c
Giovani Fernandes <giovani_oli@hotmail.com>
2020-01-17 00:33:49 -0300

    function new contact added and fab-button

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -2,4 +2,25 @@ class ContactsController < ApplicationController
   def index
     @contacts = current_user.contacts
   end
+
+  def new
+    @contact = Contact.new
+  end
+
+  def create
+    user = User.find(email: contact_params[:record][:email])
+    @contact =current_user.contacts.build(record: user)
+    if contact.save
+      flash[:notice] = "Contact successfully create"
+      redirect_to contacts_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def contact_params
+    params.require(:contact).permit(record: :email)
+  end
 end

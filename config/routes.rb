e1fb03ee658e09ea74a6aa4c60f6fa26a0be3c12
Giovani Fernandes <giovani_oli@hotmail.com>
2020-01-17 01:27:03 -0300

    send message in chat

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :contacts, only: %i(index new create destroy)
-  resources :chats, only: %i(index show)
+  resources :chats, only: %i(index show) do
+    resources :messages, only: :create, defaults: { format: :js }
+  end
   root to: 'home#index'
 end
